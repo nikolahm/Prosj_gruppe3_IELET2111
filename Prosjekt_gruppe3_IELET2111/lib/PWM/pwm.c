2f8a4183c694b97a578b36619317fa07aec45673
Nicolas Astrup <146210940+Nicolas-astrup@users.noreply.github.com>
2024-04-09 15:25:13 +0200

    Update pwm.c
    Now working with 3 different motors

--- Prosjekt_gruppe3_IELET2111/lib/PWM/pwm.c
+++ Prosjekt_gruppe3_IELET2111/lib/PWM/pwm.c
@@ -13,7 +13,9 @@
 /*Using default clock 3.33MHz */
 void TCA0_init(void);
 void PORT_init(void);
-void speed_controll(uint8_t persentage);
+void speed_controll_1(uint8_t persentage);
+void speed_controll_2(uint8_t persentage);
+void speed_controll_3(uint8_t persentage);
 
 
 int main(void)
@@ -22,11 +24,13 @@ int main(void)
 	TCA0_init();
 	while (1)
 	{
-		speed_controll(5);
+		speed_controll_1(0);
+		speed_controll_2(50);
+		speed_controll_3(0);
 	}
 }
 
-void speed_controll (uint8_t persentage)
+void speed_controll_1 (uint8_t persentage)
 {
 	if (persentage > 100)
 	{
@@ -34,19 +38,59 @@ void speed_controll (uint8_t persentage)
 	} else if (persentage < 20)
 	{
 		TCA0.SINGLE.CMP0BUF = (uint16_t)((PERIOD_VALUE * 20 / 100));
-	}else
+		if (persentage == 0){
+			TCA0.SINGLE.CMP0BUF = (uint16_t)((PERIOD_VALUE * 1 / 100));
+		}
+	} 
+	else
 	{
 		uint16_t speed_value = (uint16_t)((PERIOD_VALUE * persentage / 100));
 		TCA0.SINGLE.CMP0BUF = speed_value;
+	}
+}
+void speed_controll_2 (uint8_t persentage)
+{
+	if (persentage > 100)
+	{
+		TCA0.SINGLE.CMP1BUF = PERIOD_VALUE;
+	} else if (persentage < 20)
+	{
+		TCA0.SINGLE.CMP1BUF = (uint16_t)((PERIOD_VALUE * 20 / 100));
+		if (persentage == 0){
+			TCA0.SINGLE.CMP1BUF = (uint16_t)((PERIOD_VALUE * 1 / 100));
+		}
+	}
+	else
+	{
+		uint16_t speed_value = (uint16_t)((PERIOD_VALUE * persentage / 100));
+		TCA0.SINGLE.CMP1BUF = speed_value;
+	}
+}
+void speed_controll_3 (uint8_t persentage)
+{
+	if (persentage > 100)
+	{
+		TCA0.SINGLE.CMP2BUF = PERIOD_VALUE;
+	} else if (persentage < 20)
+	{
+		TCA0.SINGLE.CMP2BUF = (uint16_t)((PERIOD_VALUE * 20 / 100));
+		if (persentage == 0){
+			TCA0.SINGLE.CMP2BUF = (uint16_t)((PERIOD_VALUE * 1 / 100));
+		}
+	}
+	else
+	{
+		uint16_t speed_value = (uint16_t)((PERIOD_VALUE * persentage / 100));
+		TCA0.SINGLE.CMP2BUF = speed_value;
 	}
 }
 
 void TCA0_init(void)
 {
 	/* set waveform output on PORT D*/
-	PORTMUX.TCAROUTEA = PORTMUX_TCA0_PORTD_gc;
+	PORTMUX.TCAROUTEA = PORTMUX_TCA0_PORTD_gc | PORTMUX_TCA0_PORTE_gc;
 
-	TCA0.SINGLE.CTRLB = TCA_SINGLE_CMP0EN_bm | TCA_SINGLE_CMP1EN_bm /* enable compare
+	TCA0.SINGLE.CTRLB = TCA_SINGLE_CMP0EN_bm | TCA_SINGLE_CMP1EN_bm |TCA_SINGLE_CMP2EN_bm /* enable compare
 	channel 0 */
 	| TCA_SINGLE_WGMODE_SINGLESLOPE_gc; /* set dual-slope PWM
 	mode */
@@ -67,6 +111,7 @@ void PORT_init(void)
 	/* set pin 0 of PORT D as output */
 	PORTD.DIRSET |= PIN0_bm;
 	PORTD.DIRSET |= PIN1_bm;
+	PORTD.DIRSET |= PIN2_bm;
 }
 
 
