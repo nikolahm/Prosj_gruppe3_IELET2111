6d850a07bc82f7356473f54c831327e894a46495
Haakonsater <39953946+Haakonsater@users.noreply.github.com>
2024-04-11 21:18:50 +0200

    Update statemachine.h

--- Prosjekt_gruppe3_IELET2111/lib/States/statemachine.h
+++ Prosjekt_gruppe3_IELET2111/lib/States/statemachine.h
@@ -1,19 +1,24 @@
 /*
- * statemachine.h
+ * switchstates1.h
  *
- * Created: 10.04.2024 15:10:01
+ * Created: 11.04.2024 15:44:04
  *  Author: Meg
  */ 
 
 
-#ifndef STATEMACHINE_H_
-#define STATEMACHINE_H_
+#ifndef SWITCHSTATES1_H_
+#define SWITCHSTATES1_H_
+void fan_choice();
+void speed_controll_0();
+void speed_controll_1();
+void speed_controll_2();
+void speed_controll_3();
+void speed_controll_4();
+void speed_controll_5();
+void speed_controll_6();
+void speed_controll_7();
+void speed_controll_8();
 
-#define USART3_BAUD_RATE(BAUD_RATE) ((float)(F_CPU*64 / (16*(float)BAUD_RATE )) + 0.5)
-#define MAX_COMMAND_LEN 10
-#define NUMBER_OF_COMMANDS 8
-#define CR 0x0D // ’\r’
-#define LF 0x0A // ’\n’
 
 //prototypes for the switch case
 void automaticspeed();
@@ -26,32 +31,27 @@ uint8_t start_var;
 uint8_t modes_var;
 uint8_t manual_var;
 uint8_t fan_var;
+uint8_t data_var;
+uint8_t fanspeed_val;
+static bool set_speed_flag = false;
 //percentage speed for the fans
 int percentage_val;
 
-char command [ MAX_COMMAND_LEN ]; // max lenght 
-bool auto_flag;
-int speed;
-uint8_t index = 0;
-uint8_t data_ready_flag= 0;
+static char *choicearray[9]={
 
-char c; //interupt variable for uart in loop
-volatile char recived; //interupt variable for uart
-// Prototypes for uart
-void USART3_init ( void );
-void USART3_sendChar ( char c);
-static int USART3_printChar ( char c, FILE * stream );
-void USART3_sendString ( char * str );
-uint8_t USART3_read ();
-
-uint8_t compareCommands ( char* src,char* arrays[],uint8_t array_size);
-uint8_t case_choice(char*src,char choice);
-
-// Setting up FDEV_SETUP_STREAM to use USART3_printChar
-static FILE USART_stream = FDEV_SETUP_STREAM ( USART3_printChar , NULL ,_FDEV_SETUP_WRITE );
-// Array of commands to compare to
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9"
+};
 
 
 
 
-#endif /* STATEMACHINE_H_ */
+
+#endif /* SWITCHSTATES1_H_ */
