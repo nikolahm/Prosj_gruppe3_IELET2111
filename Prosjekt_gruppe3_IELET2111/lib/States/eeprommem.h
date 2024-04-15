da12460c191616916768f15eef9029ee7eaf2b4d
Haakonsater <39953946+Haakonsater@users.noreply.github.com>
2024-04-15 15:48:39 +0200

    Update eeprommem.h

--- Prosjekt_gruppe3_IELET2111/lib/States/eeprommem.h
+++ Prosjekt_gruppe3_IELET2111/lib/States/eeprommem.h
@@ -1,3 +1,4 @@
+
 /*
  * eeprommem.h
  *
@@ -8,25 +9,40 @@
 
 #ifndef EEPROMMEM_H_
 #define EEPROMMEM_H_
-uint16_t start_fan_adress1= 0x1400;
-uint16_t start_fan_adress2= 0x1430;
-uint16_t start_fan_adress3= 0x1460;
-uint16_t start_fan_adress4= 0x1490;
-uint16_t start_fan_adress5= 0x14C0;
-uint16_t start_fan_adress6= 0x14F0;
-uint16_t start_fan_adress7= 0x1520;
-uint16_t start_fan_adress8= 0x1550;
 
-uint16_t current_fan_adress1=	;
-uint16_t current_fan_adress2=   ;
-uint16_t current_fan_adress3=   ;
-uint16_t current_fan_adress4=   ;
-uint16_t current_fan_adress5=   ;
-uint16_t current_fan_adress6=   ;
-uint16_t current_fan_adress7=   ;
-uint16_t current_fan_adress8=   ;
+	//the starting adress of the fans, these start adresses contains information on how many of the adresses is ocuupied
+static uint16_t start_fan_adress1= 0x1400;
+static uint16_t start_fan_adress2= 0x1430;
+static uint16_t start_fan_adress3= 0x1460;
+static uint16_t start_fan_adress4= 0x1490;
+static uint16_t start_fan_adress5= 0x14C0;
+static uint16_t start_fan_adress6= 0x14F0;
+static uint16_t start_fan_adress7= 0x1520;
+static uint16_t start_fan_adress8= 0x1550;
 
-int i;
+
+
+static uint16_t current_fan_adress1= 0x1401; //
+static uint16_t current_fan_adress2= 0x1431;
+static uint16_t current_fan_adress3= 0x1461;
+static uint16_t current_fan_adress4= 0x1491;
+static uint16_t current_fan_adress5= 0x14C1;
+static uint16_t current_fan_adress6= 0x14F1;
+static uint16_t current_fan_adress7= 0x1521;
+static uint16_t current_fan_adress8= 0x1551;
+
+static uint16_t n_count_adress1=1;
+static uint16_t n_count_adress2=1;
+static uint16_t n_count_adress3=1;
+static uint16_t n_count_adress4=1;
+static uint16_t n_count_adress5=1;
+static uint16_t n_count_adress6=1;
+static uint16_t n_count_adress7=1;
+static uint16_t n_count_adress8=1;
+
+
+
+static int i = 0;
 int store_rpm1;
 int store_rpm2;
 int store_rpm3;
@@ -35,9 +51,9 @@ int store_rpm5;
 int store_rpm6;
 int store_rpm7;
 int store_rpm8;
-bool write_eeprom_flag = false; 
+static bool write_eeprom_flag = false; 
 
-void avrage_rpm();
+
 typedef struct{
 	uint8_t rpm;
 	}config_t; 
