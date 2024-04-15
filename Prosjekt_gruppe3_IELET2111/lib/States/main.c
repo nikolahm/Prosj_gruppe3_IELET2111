6a7adc4e40194bfaaa0b254f19144f3723fec6cf
Haakonsater <39953946+Haakonsater@users.noreply.github.com>
2024-04-15 15:49:13 +0200

    Update main.c

--- Prosjekt_gruppe3_IELET2111/lib/States/main.c
+++ Prosjekt_gruppe3_IELET2111/lib/States/main.c
@@ -1,5 +1,5 @@
 /*
- * sttstst.c
+ * statemachine.c
  *
  * Created: 21.03.2024 11:18:08
  * Author : Meg
@@ -17,39 +17,44 @@
 #include <avr/eeprom.h>
 #include "uart.h"
 #include "switchstates1.h"
-
+#include "eeprommem.h"
+#include "PWM.h"
 
 
 int main ( void )
 {
+	PORT_init();
+	TCA0_init();
 	USART3.CTRLA |= USART_RXCIE_bm ;
 	USART3_init ();
 	/* This delay invalidates the initial noise on the TX line , after device reset
 	. */
 	 sei();
 	_delay_ms (10) ;
+	
+	FUSE.SYSCFG0 |=(0<<0);// eeprom is saved under chip erase
+	eeprom_write_byte(start_fan_adress1,10);
+	
 	while (1)
 	{
-		
-			if (data_ready_flag) {
+		if (data_ready_flag) {
 			c = recived; // Update 'c' with the received character
 			data_ready_flag = 0;
-			 // Process received_char as needed
-			 if(c != LF && c != CR){ // If received \r or \n
-				 command[index++] = c;
-				 if( index > MAX_COMMAND_LEN )
-				 {
-					 printf ("\r\n");
-					 index = 0;
-				 }
-			 }
-			 if(c == CR || c == '\n'){ // if recived \r
-				 command [index] = 0;
-				 index = 0;
-				 
-				 startMenu();
-							
-			}	
+			// Process received_char as needed
+			if(c != LF && c != CR){ // If received \r or \n
+				command[index++] = c;
+				if( index > MAX_COMMAND_LEN )
+				{
+					printf ("\r\n");
+					index = 0;
+				}
+			}
+			if(c == CR || c == '\n'){ // if recived \r
+				command [index] = 0;
+				index = 0;
+		
+		startMenu();	
+			}
 		}
 	}
 }
@@ -59,3 +64,4 @@ ISR (USART3_RXC_vect){
 	USART3.STATUS |= USART_RXCIF_bm;
 	data_ready_flag = 1;
 }
+
