74a50a187c30e9488cfc3c89ddf96dce59a0fb3c
Haakonsater <39953946+Haakonsater@users.noreply.github.com>
2024-04-23 14:55:52 +0200

    Create PWM.h

---
+++ Prosjekt_gruppe3_IELET2111/lib/main/PWM.h
@@ -0,0 +1,30 @@
+
+ * PWM.h
+ *
+ * Created: 15.04.2024 15:31:23
+ *  Author: Meg
+ */ 
+
+
+#ifndef PWM_H_
+#define PWM_H_
+
+
+
+void TCA0_init(void);
+void TCA1_init(void);
+void PORT_init(void);
+void speed_controll_0(uint8_t persentage);
+void speed_controll_1(uint8_t persentage);
+void speed_controll_2(uint8_t persentage);
+void speed_controll_3(uint8_t persentage);
+void speed_controll_4(uint8_t persentage);
+void speed_controll_5(uint8_t persentage);
+void speed_controll_6 (uint8_t persentage);
+void speed_controll_7 (uint8_t persentage);
+
+
+
+
+
+#endif /* PWM_H_ 
