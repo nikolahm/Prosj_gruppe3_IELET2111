5dc3b8cf725dd6700755c0687b257de86533838e
Haakonsater <39953946+Haakonsater@users.noreply.github.com>
2024-04-24 16:52:42 +0200

    Update main.c

--- Prosjekt_gruppe3_IELET2111/lib/main/main.c
+++ Prosjekt_gruppe3_IELET2111/lib/main/main.c
@@ -20,10 +20,14 @@
 #include "eeprommem.h"
 #include "PWM.h"
 #include "RPM.h"
+#include "twi_aht10.h"
+
+volatile uint16_t signal_pulse = 0;
+volatile uint16_t signal_period = 0;
+volatile uint16_t rpm_from_inter;
 
 
-
-
+uint16_t varvar;
 
 int main ( void )
 {
@@ -33,12 +37,16 @@ int main ( void )
 	USART3_init ();
 	/* This delay invalidates the initial noise on the TX line , after device reset
 	. */
-	
-	
-	 sei();  //activate global interupts
-	 initialize_adresses();
+	 
+	 EVSYS_init();
+	 TCB0_init();
+	 		
+	 PORTB.DIR &= ~PIN5_bm;
+	 PORTB.PIN5CTRL |= PORT_PULLUPEN_bm;
+	 
 	_delay_ms (10) ;
 	initialize_rpm();
+	sei();  //activate global interupts
 	_delay_ms (10) ;
 	twi_master_init_aht10();
 	_delay_ms (1000);
@@ -48,13 +56,22 @@ int main ( void )
 	// FUSE.SYSCFG0 |=(0<<1); // eeprom is saved under chip erase, optional
 	
 	while (1)
-	{ 
-		RPM_value();
-		automaticspeed();
-		average_rpm(RPM,&current_fan_adress1,&store_rpm1,&n_count_adress1,&start_fan_adress1);
+	{	
+		//cli();
+		//_delay_ms(10);
+		
+		automaticspeed();//needs to be updated always
 
-		if (1==1/*data_ready_flag*/) { //if interupt on usart has happend, change to data ready flag if interupt is used
-			recived = USART3_read();
+average_rpm(RPM,&current_fan_adress1,&store_rpm1,&n_count_adress1,&start_fan_adress1);
+// 		
+// 		printf("int: ");
+// 		
+// 		printf("%d\n",signal_pulse);
+		//////////////////////////////////////////////////////////////////
+		
+
+		if (data_ready_flag) { //if interupt on usart has happend, change to data ready flag if interupt is used
+ 			//recived = USART3_read();
 			c = recived; // Update 'c' with the received character
 			data_ready_flag = 0; //disable so it only runs once each message and iteration
 			// Process received_char as needed
@@ -76,22 +93,52 @@ int main ( void )
 		
 			}
 		}
-		
+		//sei();
 	}
 	
 	
 }
 
-/*ISR (USART3_RXC_vect){
-	recived = USART3_read(); //variable containing information about the message
-	USART3.STATUS |= USART_RXCIF_bm; //usart status set to clear, so information can be recived next interupt
-	data_ready_flag = 1; //set flag to recived information on usart
-}*/
+
 
 ISR(TCB0_INT_vect)
 {
+	cli();
 	signal_pulse = TCB0.CCMP/4;
 	signal_period = TCB0.CNT/4;
+	RPM = (60.0/(2.0*0.00001*((float)signal_pulse/5)));
 			// Reset interrupt flag
 	TCB0.INTFLAGS = TCB_CAPT_bm;
+//	printf("%d\n",rpm_from_inter);
+	sei();
+	
 }
+
+int RPMstuff(void)
+{
+	if (!(TCB0.INTFLAGS))
+	{
+		
+		char pulseStr[10];
+		sprintf(pulseStr, "%d\r\n", signal_pulse);
+		USART3_sendString("\n\nSignal pulse [us]: ");
+		USART3_sendString(pulseStr);
+		
+		char periodStr[10];
+		sprintf(periodStr, "%d\r\n", signal_period);
+		USART3_sendString("\nSignal period [us]: ");
+		USART3_sendString(periodStr);
+
+
+		
+	}
+}
+
+ISR (USART3_RXC_vect){
+	recived = USART3_read(); //variable containing information about the message
+	USART3.STATUS |= USART_RXCIF_bm; //usart status set to clear, so information can be recived next interupt
+	data_ready_flag = 1; //set flag to recived information on usart
+}
+
+
+
