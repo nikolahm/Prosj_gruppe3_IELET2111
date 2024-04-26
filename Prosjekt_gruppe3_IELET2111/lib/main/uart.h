15baf7574f15804180af04d574a4123693a3ccf6
Haakonsater <39953946+Haakonsater@users.noreply.github.com>
2024-04-26 19:41:36 +0200

    Update uart.h

--- Prosjekt_gruppe3_IELET2111/lib/main/uart.h
+++ Prosjekt_gruppe3_IELET2111/lib/main/uart.h
@@ -15,18 +15,19 @@
 #define LF 0x0A // ’\n’
 #include <stdbool.h>
 
-volatile char recived; //interupt variable for uart
-char command [ MAX_COMMAND_LEN ]; // max lenght
-char c; //interupt variable for uart in loop
 
+char command [ MAX_COMMAND_LEN ]; // max lenght
+bool auto_flag;
 int speed;
 static uint8_t index = 0;
-static volatile uint8_t data_ready_flag= 0;
+static uint8_t data_ready_flag= 0;
 
+char c; //interupt variable for uart in loop
+volatile char recived; //interupt variable for uart
 // Prototypes for uart
 void USART3_init ( void );
 void USART3_sendChar ( char c);
- int USART3_printChar ( char c, FILE * stream ); //defined used but not defined?
+static int USART3_printChar ( char c, FILE * stream );
 void USART3_sendString ( char * str );
 uint8_t USART3_read ();
 
@@ -34,7 +35,7 @@ uint8_t compareCommands ( char* src,char* arrays[],uint8_t array_size);
 uint8_t case_choice(char*src,char choice);
 
 // Setting up FDEV_SETUP_STREAM to use USART3_printChar
-static FILE USART_stream = FDEV_SETUP_STREAM ( USART3_printChar , NULL ,_FDEV_SETUP_WRITE ); //defined but not used
+static FILE USART_stream = FDEV_SETUP_STREAM ( USART3_printChar , NULL ,_FDEV_SETUP_WRITE );
 // Array of commands to compare to
 
 #endif /* UART_H_ */
