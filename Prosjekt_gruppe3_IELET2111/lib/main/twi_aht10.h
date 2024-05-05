0dd51546aff76b9afabdce464b8559665a381352
Haakonsater <39953946+Haakonsater@users.noreply.github.com>
2024-05-05 13:08:58 +0200

    Update twi_aht10.h

--- Prosjekt_gruppe3_IELET2111/lib/main/twi_aht10.h
+++ Prosjekt_gruppe3_IELET2111/lib/main/twi_aht10.h
@@ -1,14 +1,16 @@
 /*
- * twi_aht10.h
+ * twi_slave.h
  *
- * Created: 22.04.2024 11:34:42
- *  Author: Meg
+ * Created: 4/19/2024 10:08:23 AM
+ *  Author: nicol
  */ 
 
 
 #ifndef TWI_AHT10_H_
 #define TWI_AHT10_H_
+
 void twi_master_init_aht10(void);
 uint8_t read_aht10_data(uint8_t mesurments);
+
 
 #endif /* TWI_AHT10_H_ */
